35872cac20f636036f212d040ebf665040883567
Samira <nezhadsamira9@gmail.com>
2025-10-16 00:13:46 -0800

    updated Identity entities and DbContext

---
+++ CCTVManagementSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CCTVManagementSystem.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CCTVManagementSystem.Infrastructure.Data;
+
+public class AppDbContext :IdentityDbContext<AppUser, AppRole, string>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+        
+    }
+}
